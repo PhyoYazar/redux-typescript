4a78047600e704deff803a07992e90f36297c9e1
Phyo Yarzar <phyoyazar592@gmail.com>
2022-10-22 23:16:10 +0630

    [Errors] Form submit 'Search' button is fetching data, but not rerender(need rerender to show data)

--- src/components/RepositoriesLists.tsx
+++ src/components/RepositoriesLists.tsx
@@ -1,19 +1,31 @@
 import React, { useState } from "react";
-import { useActions } from "../hooks/useActions";
 import { useTypedSelector } from "../hooks/useTypedSelector";
+import { useActions } from "../hooks/useActions";
+
+// import { useDispatch } from "react-redux";
+// import { actionCreators } from "../store";
 
 const RepositoriesLists: React.FC = () => {
   const [term, setTerm] = useState("");
+  // const dispatch = useDispatch();
+  // const { data, error, loading } = useTypedSelector(
+  //   (state) => state.repositories
+  // );
+
   const { searchRepositories } = useActions();
   const { data, error, loading } = useTypedSelector(
-    (state: any) => state.repositories
+    (state) => state.repositories
   );
 
+  // const onSubmitHanlder = (event: React.SyntheticEvent<HTMLFormElement>) => {
   const onSubmitHanlder = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
-    //* This is data fetching with dispatch from hooks
+    //! This component isn't rerendering if click the 'Search' button
+
+    //? This is data fetching with dispatch from hooks
     searchRepositories(term);
+    // dispatch(actionCreators.searchRepositories(term));
   };
 
   return (
@@ -22,6 +34,10 @@ const RepositoriesLists: React.FC = () => {
         <input value={term} onChange={(e) => setTerm(e.target.value)} />
         <button>Search</button>
       </form>
+
+      {error && <h3>{error}</h3>}
+      {loading && <h3>Loading ...</h3>}
+      {!error && !loading && data.map((name) => <div key={name}>{name}</div>)}
     </div>
   );
 };
--- src/store/action-creators/index.ts
+++ src/store/action-creators/index.ts
@@ -17,7 +17,7 @@ export const searchRepositories = (term: string) => {
         }
       );
 
-      const names = data.object.map((result: any) => {
+      const names = data.objects.map((result: any) => {
         return result.package.name;
       });
 
